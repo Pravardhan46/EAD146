ffa168f762de39e9a7b8421a9f6f5135051cbcc9
abhinaya1930 <abhinayagaddampally@gmail.com>
2024-08-26 12:41:40 +0530

    mongodb replica

--- server/app.js
+++ server/app.js
@@ -4,13 +4,14 @@ const router=require("./routes/routes")
 
 const app=express();
 app.use(express.json());
- 
-const url = "mongodb://127.0.0.1:27017/CBIT";
+
+const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
+// const url = "mongodb://127.0.0.1:27017/CBIT";
 mongoose
   .connect(url)
   .then(() => console.log("Successfully connected to database"))
   .catch((err) => console.log(err));
-
+app.use('/routes',router);
 app.listen(9000,()=>{
     console.log('Server started');
 })
